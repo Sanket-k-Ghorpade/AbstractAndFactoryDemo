c1d6344ec69c0b5a4a6013d39ed0e2d4c39de32d
sanket..ghorpade <sanket..ghorpade@HP2335>
2025-07-16 13:04:39 +0530

    Add project files.

---
+++ FactoryDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿// Product Interface
+public interface ICar
+{
+    void Drive();
+}
+
+// Concrete Products
+public class Sedan : ICar
+{
+    public void Drive()
+    {
+        Console.WriteLine("Driving a Sedan");
+    }
+}
+
+public class SUV : ICar
+{
+    public void Drive()
+    {
+        Console.WriteLine("Driving an SUV");
+    }
+}
+
+// Factory
+public class CarFactory
+{
+    public static ICar CreateCar(string type)
+    {
+        switch (type)
+        {
+            case "sedan": return new Sedan();
+            case "suv": return new SUV();
+            default: throw new ArgumentException("Invalid car type");
+        }
+    }
+}
+
+// Client Code
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Which type of car are you looking for: sedan or suv");
+        string choice = Console.ReadLine().ToLower();
+
+        ICar car = CarFactory.CreateCar(choice);
+        car.Drive();
+    }
+}
